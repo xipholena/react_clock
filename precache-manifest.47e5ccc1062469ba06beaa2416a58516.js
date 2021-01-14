cce8c86bd22ae9eeab6c5dcd08846a0ff5de28b2
Your Name <mogolgogol.90@gmail.com>
2021-01-14 21:30:58 +0200

    Updates

---
+++ precache-manifest.47e5ccc1062469ba06beaa2416a58516.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3da2181211d08d299bdbb174c2d0d26",
+    "url": "./index.html"
+  },
+  {
+    "revision": "81827d3fa9dcf12b0559",
+    "url": "./static/css/main.87ab5f2d.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81827d3fa9dcf12b0559",
+    "url": "./static/js/main.a36f57bb.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
